c7ffd6e30bde89d477e1038213fe958a43ff4fb1
dhtran <dh.tran@ateme.com>
2021-12-16 01:38:22 +0100

    feat: add crud api requests for comments

--- python/main.py
+++ python/main.py
@@ -219,6 +219,10 @@ class CommentBody(BaseModel):
     comment: str
 
 
+class UpdateCommentBody(BaseModel):
+    comment: str
+
+
 @app.post("/comments/episodes/{episode_id}")
 def create_comment_episode(body: CommentBody, episode_id: int) -> int:
     """
@@ -235,6 +239,92 @@ def create_comment_episode(body: CommentBody, episode_id: int) -> int:
         raise HTTPException(status_code=404, detail="Episode does not exist.")
     insert_comment_query = f"INSERT INTO comments (username, episode_id, comment) VALUES ('{body.username}', '{episode_id}', '{body.comment}')"
     return insert_into_table(insert_comment_query)
+
+
+@app.post("/comments/characters/{character_id}")
+def create_comment_character(body: CommentBody, character_id: int) -> int:
+    """
+    Create comment on a character
+    :param character_id: character id
+    :param body: body config
+    :return: comment_id
+    """
+    select_user_exists_query = f"SELECT EXISTS(SELECT username FROM users WHERE username = '{body.username}')"
+    if fetchall_results(select_user_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Username does not exist.")
+    select_character_id_exists_query = f"SELECT EXISTS(SELECT id FROM characters WHERE id = '{character_id}')"
+    if fetchall_results(select_character_id_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Episode does not exist.")
+    insert_comment_query = f"INSERT INTO comments (username, character_id, comment) VALUES ('{body.username}', '{character_id}', '{body.comment}')"
+    return insert_into_table(insert_comment_query)
+
+
+@app.post("/comments/episodes/{episode_id}/{character_id}")
+def create_comment_on_character_in_episode(body: CommentBody, episode_id: int, character_id: int) -> int:
+    """
+    Create comment on an episode
+    :param character_id: character id
+    :param episode_id: episode id
+    :param body: body config
+    :return: comment_id
+    """
+    select_user_exists_query = f"SELECT EXISTS(SELECT username FROM users WHERE username = '{body.username}')"
+    if fetchall_results(select_user_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Username does not exist.")
+    select_episode_id_exists_query = f"SELECT EXISTS(SELECT id FROM episodes WHERE id = '{episode_id}')"
+    if fetchall_results(select_episode_id_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Episode does not exist.")
+    select_characters_in_episode_query = f"SELECT characters FROM episodes WHERE id = '{episode_id}'"
+    results = fetchall_results(select_characters_in_episode_query)
+    if not results or character_id not in ast.literal_eval(results[0][0]):
+        raise HTTPException(status_code=404, detail=f"Character id {character_id} is not in episode id {episode_id}.")
+    insert_comment_query = f"INSERT INTO comments (username, episode_id, character_id, comment) VALUES ('{body.username}', '{episode_id}', '{character_id}', '{body.comment}')"
+    return insert_into_table(insert_comment_query)
+
+
+@app.put("/comments/{comment_id}")
+def update_comment_by_id(comment_id: int, body: UpdateCommentBody) -> dict:
+    """
+    Update comment by id
+    :param comment_id: comment id
+    :param body: request body
+    :return: updated comment info
+    """
+    select_comment_id_exists_query = f"SELECT EXISTS(SELECT id FROM comments WHERE id = '{comment_id}')"
+    if fetchall_results(select_comment_id_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Comment does not exist.")
+    update_comment_query = f"UPDATE comments SET comment = '{body.comment}' WHERE id = '{comment_id}'"
+    fetchall_results(update_comment_query)
+    select_comment_query = f"SELECT * FROM comments WHERE id = '{comment_id}'"
+    results = fetchall_results(select_comment_query)
+    id, username, episode_id, character_id, comment = results[0]
+    return {
+        "id": id,
+        "username": username,
+        "episode_id": episode_id,
+        "character_id": character_id,
+        "comment": comment,
+    }
+
+
+@app.get("/comments")
+def get_all_comments() -> list:
+    """
+    Get all comments
+    :return: list of comments
+    """
+    select_all_comments_query = f"SELECT * FROM comments"
+    results = fetchall_results(select_all_comments_query)
+    return [
+        {
+            "id": id,
+            "username": username,
+            "episode_id": episode_id,
+            "character_id": character_id,
+            "comment": comment,
+        }
+        for id, username, episode_id, character_id, comment in results
+    ]
 
 
 @app.get("/comments/episodes/{episode_id}")
@@ -259,3 +349,92 @@ def get_all_comments_of_an_episode(episode_id: int) -> list:
         }
         for id, username, episode_id, character_id, comment in results
     ]
+
+
+@app.get("/comments/characters/{character_id}")
+def get_all_comments_of_a_character(character_id: int) -> list:
+    """
+    Get all comments of a character
+    :param character_id: character id
+    :return: list of comments
+    """
+    select_character_id_exists_query = f"SELECT EXISTS(SELECT id FROM characters WHERE id = '{character_id}')"
+    if fetchall_results(select_character_id_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Character does not exist.")
+    select_all_comments_of_character_query = f"SELECT * FROM comments WHERE character_id = '{character_id}'"
+    results = fetchall_results(select_all_comments_of_character_query)
+    return [
+        {
+            "id": id,
+            "username": username,
+            "episode_id": episode_id,
+            "character_id": character_id,
+            "comment": comment,
+        }
+        for id, username, episode_id, character_id, comment in results
+    ]
+
+
+@app.get("/comments/episodes/{episode_id}/{character_id}")
+def get_all_comments_of_character_in_episode(episode_id: int, character_id: int) -> list:
+    """
+    Get all comments of a character in an episode
+    :param character_id: character id
+    :param episode_id: episode id
+    :return: list of comments
+    """
+    select_episode_id_exists_query = f"SELECT EXISTS(SELECT id FROM episodes WHERE id = '{episode_id}')"
+    if fetchall_results(select_episode_id_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Episode does not exist.")
+    select_characters_in_episode_query = f"SELECT characters FROM episodes WHERE id = '{episode_id}'"
+    results = fetchall_results(select_characters_in_episode_query)
+    if not results or character_id not in ast.literal_eval(results[0][0]):
+        raise HTTPException(status_code=404, detail=f"Character id {character_id} is not in episode id {episode_id}.")
+    select_all_comments_of_character_in_episode_query = (
+        f"SELECT * FROM comments WHERE episode_id = '{episode_id}' and character_id = '{character_id}'"
+    )
+    results = fetchall_results(select_all_comments_of_character_in_episode_query)
+    return [
+        {
+            "id": id,
+            "username": username,
+            "episode_id": episode_id,
+            "character_id": character_id,
+            "comment": comment,
+        }
+        for id, username, episode_id, character_id, comment in results
+    ]
+
+
+@app.get("/comments/{comment_id}")
+def get_comment_by_id(comment_id: int) -> dict:
+    """
+    Get comment by id
+    :param comment_id: comment id
+    :return: comment info
+    """
+    select_comment_id_exists_query = f"SELECT EXISTS(SELECT id FROM comments WHERE id = '{comment_id}')"
+    if fetchall_results(select_comment_id_exists_query)[0][0] == 0:
+        raise HTTPException(status_code=404, detail="Comment does not exist.")
+    select_comment_query = f"SELECT * FROM comments WHERE id = '{comment_id}'"
+    results = fetchall_results(select_comment_query)
+    id, username, episode_id, character_id, comment = results[0]
+    return {
+        "id": id,
+        "username": username,
+        "episode_id": episode_id,
+        "character_id": character_id,
+        "comment": comment,
+    }
+
+
+@app.delete("/comments/{comment_id}")
+def delete_comment_by_id(comment_id: int) -> str:
+    """
+    Delete comment by id
+    :param comment_id: comment id
+    :return: delete message
+    """
+    select_comment_query = f"DELETE FROM comments WHERE id = '{comment_id}'"
+    fetchall_results(select_comment_query)
+    return f"Comment id {comment_id} has been deleted."
